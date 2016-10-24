79f5fad716365916c51bdb8ddd2a206ed1870147
MHenintsoa <tahiana@wau.solutions>
2016-10-24 16:49:40 +0300

    infoCustomer

--- application/routes.php
+++ application/routes.php
@@ -4,26 +4,4 @@ $app->get('/[home]', 'App\Http\Controller\WelcomeController@index');
 $app->get('/info', 'App\Http\Controller\WelcomeController@info');
 
 
-/**
- * Developper resources
- * Examples of grouped routing and useing middlewares
- *
-$app->group(['prefix' => '/developer', 'middleware' => ['Wau\Podio\PodioOAuthMiddleware'],
-             function() use (&$app) {
-                 $app->get(['/server.info', 'server.info'],
-                     'Wau\Http\Controller\DeveloperController@serverInfo'
-                 );
-                 $app->get(['/app', 'developer.app'],
-                     'Wau\Http\Controller\DeveloperController@printApp'
-                 );
-                 $app->get(['/logs', 'developer.logs'],
-                     'Wau\Http\Controller\DeveloperController@listLog'
-                 );
-                 $app->get(['/logs-{name}', 'developer.log'],
-                     'Wau\Http\Controller\DeveloperController@viewLog'
-                 );
-             }
-    ]
-);
- *
- */
+
