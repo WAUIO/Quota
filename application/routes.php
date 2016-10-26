d399a5298cc5b50224cdb6193626af45927fd54f
ninah hexadec <ninah@wau.solutions>
2016-10-26 13:49:12 +0200

    fing problem

--- application/routes.php
+++ application/routes.php
@@ -1,12 +1,12 @@
 <?php
 
-$app->group(['prefix'=>'/', 'middleware' => ['App\Http\Middleware\AuthMiddleware'],
-    function() use (&$app){
-        $app->get('/form', 'App\Http\Controller\WelcomeController@index');
+//$app->group(['prefix'=>'/', 'middleware' => ['App\Http\Middleware\AuthMiddleware'],
+//    function() use (&$app){
+//        $app->get('/form', 'App\Http\Controller\WelcomeController@index');
+//
+//    }
+//]);
 
-    }
-]);
-
-$app->get('/[home]', 'App\Http\Controller\WelcomeController@index');
+$app->get('/form', 'App\Http\Controller\WelcomeController@index');
 $app->get('/info', 'App\Http\Controller\WelcomeController@info');
 $app->get('/', 'App\Http\Controller\QuotaViewController@index');
