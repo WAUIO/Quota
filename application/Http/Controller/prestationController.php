8979ddac908e79c73801778782c02304d73747d6
MHenintsoa <tahiana@wau.solutions>
2016-11-14 15:31:20 +0200

    saveCustomer

--- application/Http/Controller/prestationController.php
+++ application/Http/Controller/prestationController.php
@@ -8,5 +8,7 @@ use App\Utils\RoomBase;
 class prestationController extends Controller
 {
     public function prestation(){
-        return $this->app()->make('twig.view')->render('prestation.twig');    }
+
+        return $this->app()->make('twig.view')->render('prestation.twig');
+    }
 }
