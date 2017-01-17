c999be27de75a6a95dc7a8d8087032b7571f40b2
anthonywii <anthony@wau.solutions>
2017-01-17 14:36:22 +0300

    commitndray

--- config/podio.php
+++ config/podio.php
@@ -1,10 +1,8 @@
 <?php
 
 return [
-
     'CLIENT_ID'     => 'CLIENT_ID',
     'CLIENT_SECRET' => 'CLIENT_SECRET',
     'USERNAME'      => 'podio_user@email.co',
     'PASSWORD'      => 'your_password',
-
 ];
